d84b8727e67f2855da9648d195a93db4f865e2f9
Shreymish <134852152+Shreymish@users.noreply.github.com>
2025-06-28 19:45:47 +0000

    first commit

---
+++ JavaBasics.java
@@ -0,0 +1,13 @@
+public class JavaBasics{
+    public static void main(String args[]){
+        System.out.print("hello shrey\n hello shrey ");
+        System.out.print("hello shrey\n  ");
+        System.out.print("hello shrey\n ");
+
+
+
+
+
+
+    }
+}
---
+++ j.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class j {
+    public static void main(String args[]){
+       /*  System.out.println(" ****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");*/
+        /*int a = 10;
+        int b=5;
+        System.out.println(a);
+        String name ="shrey";
+        System.out.print(name);*/
+        /*byte b= 8;
+        System.out.println(b);
+        char ch = 'a';
+        System.out.println(ch);
+        float price =10.5f;
+        System.out.print(price);*/
+        //int a = 10;
+        //int b = 5;
+        //int sum = a + b;
+        //System.out.print(sum);
+     Scanner sc = new Scanner(System.in);
+     /*String input = sc.next();
+     System.out.println(input);*/
+     /*String name = sc.nextLine();
+     System.out.println(name);*/
+     //int number = sc.nextInt();
+     
+     float b = sc.nextInt();
+     float triangle = 3.14f*b*b;
+     System.out.print(triangle); 
+
+
+
+    }
+    
+}
