9c293be0f3e43998bf3658b5fd149ec3422caa18
Shreymish <134852152+Shreymish@users.noreply.github.com>
2025-06-29 11:17:00 +0000

    {commit_message}

--- conditional.java
+++ conditional.java
@@ -1,12 +1,29 @@
+import java.util.*;
 public class conditional {
     public static void main (String args[]){
-        int age = 11;
+        /*int age = 11;
         if (age>=18){
             System.out.println("adult drive , vote");
                }
         else{
-            System.out.println("not adult");
+            System.out.println("not adult"); */
+        /*Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        if (number % 2 == 0 ){
+            System.out.println("even");
+        } else {
+            System.out.println("odd");*/
+        int age = 13;
+        if(age>=18){
+            System.out.println("adult");
         }
-    }
+        else if(age>=13 && age<18){
+            System.out.println("teenage");
+        }
+        else{
+            System.out.println("child");
+        }
+        }
+        }
+        
     
-}
