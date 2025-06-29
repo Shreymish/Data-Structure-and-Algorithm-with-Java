332fa71b48685123ad5b52470e3a596271f2afee
Shreymish <134852152+Shreymish@users.noreply.github.com>
2025-06-29 14:00:19 +0000

    conditional

--- conditional.java
+++ conditional.java
@@ -13,7 +13,7 @@ public class conditional {
             System.out.println("even");
         } else {
             System.out.println("odd");*/
-        int age = 13;
+        /*int age = 13;
         if(age>=18){
             System.out.println("adult");
         }
@@ -21,9 +21,46 @@ public class conditional {
             System.out.println("teenage");
         }
         else{
-            System.out.println("child");
+            System.out.println("child");*/
+       /*  int number= 5;
+        String type =((number%2)==0)? "even" : "odd";
+        System.out.println(type);*/
+
+        // Ternary Operator 
+        /*int marks =34;
+        String RC = marks>=33? "pass" : "fail";
+        System.out.println(RC);*/
+
+        // Switch Case
+        /*int number =3 ;
+        switch (number){
+            case 1: System.out.println("smosa");
+            break;
+            case 2 : System.out.println("burger");
+            break;
+            case 3 : System.out.println("pizza");
+            break;
+            default : System.out.println("we wake up");*/
+
+            // Calculator using switch case
+           /*  Scanner sc = new Scanner(System.in);
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            char operator = sc.next().charAt(0);
+
+            switch(operator){
+                case '+' : System.out.println(a+b);
+                break;
+                case '-' : System.out.println(a-b);
+                break;
+                case '*' : System.out.println(a*b);
+                break;
+                default : System.out.println("wrong operator");*/
+            }
+
         }
+
         }
-        }
+        
         
     
